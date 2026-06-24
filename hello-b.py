5784c3556f60db95326651071f936149b18fa774
ChanLayKo <chanlayko125@gmail.com>
2026-06-24 21:51:52 +0900

    Add commit 3Line

--- hello-b.py
+++ hello-b.py
@@ -1,1 +1,3 @@
 print("hello, git-b.py")
+print("This is a simple Python script.")
+print("It demonstrates basic print functionality.")
--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print("Hello, from feature branch!")
+print("This is a simple Python script.")
+print("It demonstrates basic print functionality.")
